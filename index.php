1662df40401e20e33d3e78cdff76cdde3145ff0a
markanthonyuy <macmac.uy@gmail.com>
2014-07-18 19:02:58 +0800

    use php

---
+++ index.php
@@ -0,0 +1,207 @@
+<?php include './includes/api.php'; ?>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Laban Pilipinas</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+        <link rel="shortcut icon" type="image/png" href="public/image/labanpilipinas.ico">
+        <link rel="favicon" type="image/png" href="public/image/labanpilipinas.ico">
+        <link rel="stylesheet" href="public/css/normalize.css">
+        <link rel="stylesheet" href="public/css/main.css">
+        <script src="public/js/vendor/modernizr-2.8.0.min.js"></script>
+        <script src="http://jwpsrv.com/library/f72N8OqYEeKr4yIACqoQEQ.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div id="main">
+            <header>
+                <nav id="main_header_nav">
+                    <p class="social_media">
+                        <a href="https://www.facebook.com/Sports5PH"><img src="public/image/social-media-tab-fb.jpg"></a>
+                        <a href="https://twitter.com/sports5PH"><img src="public/image/social-media-tab-twitter.jpg"></a>
+                    </p>
+                    <ul>
+                        <li><a href="#gilas_games"><img src="public/image/link-gilas-games.jpg" alt="GILAS GAMES"></a></li>
+                        <li><a href="#rosters"><img src="public/image/link-rosters.jpg" alt="ROSTERS"></a></li>
+                        <li><a href="#photo_gallery"><img src="public/image/link-photo-gallery.jpg" alt="PHOTO GALLERY"></a></li>
+                        <li><a href="#social_media"><img src="public/image/link-social-media.jpg" alt="SOCIAL MEDIA"></a></li>
+                        <li><a href="#kampihan_wall"><img src="public/image/link-kampihan-wall.jpg" alt="KAMPIHAN WALL"></a></li>
+                    </ul>
+                </nav>
+                <section id="main_stream">
+                    <nav class="clearfix">
+                        <ul id="main_stream_nav" class="clearfix">
+                            <li><a href="#" class="stream_tv5_active" data-target="tv5" data-default-class="stream_tv5"></a></li>
+                            <li><a href="#" class="stream_aksyontv" data-target="aksyontv" data-default-class="stream_aksyontv"></a></li>
+                            <li><a href="#" class="stream_gilas" data-target="gilas" data-default-class="stream_gilas"></a></li>
+                        </ul>
+                    </nav>
+                    <div id="main_frame_holder">
+                        <!-- <iframe src="http://news5.com.ph/embed.aspx?g=0A999CBBC5" id="iframe_tv5"></iframe> -->
+                    </div>
+                </section>
+            </header>
+            <section id="gilas_games">
+                <h2><img src="public/image/heading-gilas-games.png" alt="GILAS GAMES"></h2>
+                <div class="gilas_games_wrap">
+                    <nav>
+                        <ul class="clearfix">
+                            <li>
+                                <a href="javascript:void(0)" class="tab-gilas-games" data-target="gilas_upcoming_games"><img src="public/image/tab-upcoming-games-active.jpg"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="tab-gilas-games" data-target="gilas_past_games"><img src="public/image/tab-past-games.jpg"></a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div id="gilas_upcoming_games" class="gilas_tab">
+                        <p id="gilas_team_logo"><img src="public/image/logo-gilas-pilipinas.png"></p>
+                        <p id="gilas_team_flag"><img src="public/image/flag-pilipinas-big.jpg"></p>
+                        <table>
+                            <colgroup>
+                                <col width="40%"></col>
+                                <col width="30%"></col>
+                                <col width="7%"></col>
+                                <col width="10%"></col>
+                                <col width="13%"></col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS SINGAPORE</td>
+                                    <td class="status">LOSE</td>
+                                    <td class="score">298-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS JAPAN</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div id="gilas_past_games" class="gilas_tab">
+                        <p id="gilas_team_logo"><img src="public/image/logo-gilas-pilipinas.png"></p>
+                        <p id="gilas_team_flag"><img src="public/image/flag-pilipinas-big.jpg"></p>
+                        <table>
+                            <colgroup>
+                                <col width="40%"></col>
+                                <col width="30%"></col>
+                                <col width="7%"></col>
+                                <col width="10%"></col>
+                                <col width="13%"></col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td class="date">JULY 12, 2014 VS CHINESE TAIPEI</td>
+                                    <td class="status">WIN</td>
+                                    <td class="score">98-96</td>
+                                    <td class="flag"><img src="public/image/flags_russia.jpg"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="ads">
+                </div>
+            </section>
+
+            <section id="rosters">
+                
+            </section>
+
+            <section id="photo_gallery">
+                
+            </section>
+
+            <section id="social_media">
+                
+            </section>
+
+            <section id="kampihan_wall">
+                
+            </section>
+            <footer>
+                
+            </footer>
+        </div>
+
+        <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="public/js/vendor/jquery-1.11.1.min.js"><\/script>')</script> -->
+        <script src="public/js/vendor/jquery-1.11.1.min.js"></script>
+        <script src="public/js/plugins.js"></script>
+        <script src="public/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <!-- <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script> -->
+    </body>
+</html>
